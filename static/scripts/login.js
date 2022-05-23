96226a76d9af0f9208696d46983f769842d7df6d
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-05-23 22:06:01 +0300

    beta version of messenger is here :)

---
+++ static/scripts/login.js
@@ -0,0 +1,44 @@
+import { showError } from "./modules/loginError.js";
+import { form, login, loginError, password, passwordError } from "./modules/variables.js";
+
+form.addEventListener('submit', function (event) {
+
+    login.addEventListener('input', function (event) {
+  
+        if (login.validity.valid) {
+          loginError.textContent = ''; 
+          loginError.className = 'error'; 
+        } 
+        else {
+          showError();
+        }
+
+    });
+
+    password.addEventListener('input', function (event) {
+
+        if (password.validity.valid) {
+          passwordError.textContent = ''; 
+          passwordError.className = 'error'; 
+        } 
+        else {
+          showError();
+        }
+
+    });
+  
+    if(!login.validity.valid) {
+      showError();
+      event.preventDefault();
+    }
+    else if(!password.validity.valid) {
+      showError();
+      event.preventDefault();
+    }    
+    else {
+      window.location.href = "main.html"
+      event.preventDefault();
+    }
+
+});
+
---
+++ static/scripts/sign-up.js
@@ -0,0 +1,108 @@
+import { signUpError } from "./modules/signUpError.js"
+import { form, login, loginError, password, passwordError, email, emailError, phone, phoneError, firstName, firstNameError, secondName, secondNameError } from "./modules/variables.js"
+
+form.addEventListener('submit', function (event) {
+
+    login.addEventListener('input', function () {
+  
+        if (login.validity.valid) {
+          loginError.textContent = ''; 
+          loginError.className = 'error'; 
+        } 
+        else {
+          signUpError();
+        }
+
+    });
+
+    password.addEventListener('input', function () {
+
+        if (password.validity.valid) {
+          passwordError.textContent = ''; 
+          passwordError.className = 'error'; 
+        } 
+        else {
+          signUpError();
+        }
+
+    });
+
+    email.addEventListener('input', function () {
+
+        if (email.validity.valid) {
+          emailError.textContent = ''; 
+          emailError.className = 'error'; 
+        } 
+        else {
+          signUpError();
+        }
+
+    });
+
+    phone.addEventListener('input', function () {
+
+        if (phone.validity.valid) {
+          phoneError.textContent = ''; 
+          phoneError.className = 'error'; 
+        } 
+        else {
+          signUpError();
+        }
+
+    });
+
+    first_name.addEventListener('input', function () {
+
+        if (firstName.validity.valid) {
+          firstNameError.textContent = ''; 
+          firstNameError.className = 'error'; 
+        } 
+        else {
+          signUpError();
+        }
+
+    });
+
+    second_name.addEventListener('input', function () {
+
+        if (secondName.validity.valid) {
+          secondNameError.textContent = ''; 
+          secondNameError.className = 'error'; 
+        } 
+        else {
+          signUpError();
+        }
+
+    });
+  
+    
+    if(!login.validity.valid) {
+      signUpError();
+      event.preventDefault();
+    }
+    else if(!password.validity.valid) {
+      signUpError();
+      event.preventDefault();
+    }
+    else if(!email.validity.valid) {
+      signUpError();
+      event.preventDefault();
+    }
+    else if(!firstName.validity.valid) {
+      signUpError();
+      event.preventDefault();
+    }  
+    else if(!secondName.validity.valid) {
+      signUpError();
+      event.preventDefault();
+    }  
+    else if(!phone.validity.valid) {
+      signUpError();
+      event.preventDefault();
+    }        
+    else {
+      window.location.href = "index.html"
+      event.preventDefault();
+    }
+
+});

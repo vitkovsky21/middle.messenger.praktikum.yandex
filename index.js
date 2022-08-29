e72a5233a45564720b7783442ed7fbb0d19ddff2
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-08-29 14:09:40 +0300

    index.js changed

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'dist')));
-
 
 app.listen(PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
 });
-
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "dist/index.html"));
